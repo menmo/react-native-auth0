267febcc0ab2a8c89de5c1bb3528b185c554d859
Håkan <hakan.johansson@djakne.com>
2018-04-18 15:23:46 +0200

    Test

--- android/src/main/java/com/auth0/react/A0Auth0Module.java
+++ android/src/main/java/com/auth0/react/A0Auth0Module.java
@@ -54,19 +54,20 @@ public class A0Auth0Module extends ReactContextBaseJavaModule implements Lifecyc
 
     @ReactMethod
     public void showUrl(String url, boolean closeOnLoad, Callback callback) {
-        final Activity activity = getCurrentActivity();
-
-        this.callback = callback;
-        if (activity != null) {
-            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-            CustomTabsIntent customTabsIntent = builder.build();
-            customTabsIntent.launchUrl(activity, Uri.parse(url));
-        } else {
-            final Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse(url));
-            getReactApplicationContext().startActivity(intent);
-        }
+        system.out.print("XXX japp!");
+//        final Activity activity = getCurrentActivity();
+//
+//        this.callback = callback;
+//        if (activity != null) {
+//            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+//            CustomTabsIntent customTabsIntent = builder.build();
+//            customTabsIntent.launchUrl(activity, Uri.parse(url));
+//        } else {
+//            final Intent intent = new Intent(Intent.ACTION_VIEW);
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            intent.setData(Uri.parse(url));
+//            getReactApplicationContext().startActivity(intent);
+//        }
     }
 
     @ReactMethod
